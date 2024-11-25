0520e81d330eafb1da3eeec135054bebfaf363dc
Andreas Lindström <h24andrl@du.se>
2024-11-25 09:47:45 +0100

    Change to hello world

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,14 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            try
-            {
-                int age = Int32.Parse(Console.ReadLine());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error '{e.Message}'");
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
