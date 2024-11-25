413bd75ee6f397f8f5a5948112aa72c1430a8a13
zetit <zetit@GIZMO>
2024-11-25 09:23:41 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int age = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error '{e.Message}'");
+            }
+        }
+    }
+}
